8fd347eecd2a44f7a6433bac5e1a7d62d6feb862
62262 <marcorodriguez2645@gmail.com>
2017-04-21 15:45:03 -0700

    Bitcoin Price Checker
    Checks the bitcoin price every 5 seconds.

---
+++ Price Checker/Price Checker/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+using System.Web;
+namespace Price_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.cryptonator.com/api/ticker/btc-usd");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            System.Net.WebClient wc = new System.Net.WebClient();
+            byte[] price = wc.DownloadData("http://www.yoursite.com/resource/file.htm");
+
+            string webData = System.Text.Encoding.UTF8.GetString(price);
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            richTextBox1.Text = sr.ReadToEnd();
+            sr.Close();
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    
+}
